73bb67766161af30c52d3af112c749452fd37eed
varunx007 <varunreddy.b2021@vitstudent.ac.in>
2023-10-23 21:44:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+// KindaCode.com
+// src/App.js
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+ 
+  const [text, setText] = useState('');
+
+  const [wordCount, setWordCount] = useState(0);
+
+
+  const changeHandler = (event) => {
+    setText(event.target.value);
+  };
+
+  useEffect(() => {
+   
+    const words = text.split(' ');
+
+    let wordCount = 0;
+    words.forEach((word) => {
+      if (word.trim() !== '') {
+        wordCount++;
+      }
+    });
+    setWordCount(wordCount);
+
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div><h1 id='h1'>Responsive Paragraph Word</h1></div>
+      <div className='row'><h1 id='h1'>Counter</h1></div>
+      <div>
+        <textarea
+          value={text}
+          onChange={changeHandler}
+          placeholder='write here'
+        ></textarea>
+
+        <div>
+          <p className='word-count'>Word Count: {wordCount}</p>
+          
+        </div>
+      </div>
     </div>
   );
 }
